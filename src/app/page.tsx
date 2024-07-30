705003f48d92c7dcc7f5a0f51e3c1e0d008e036c
baewoong <yoonseok.bae98@gmail.com>
2024-07-31 00:09:45 +0900

    chore: Home 컴포넌트 레이아웃 및 디자인 업데이트
    - 헤더와 푸터의 고정 위치를 제거했습니다.
    - 헤더 레이아웃 및 스타일을 업데이트했습니다.
    - 메인 콘텐츠 영역에 모의 윈도우 컨트롤을 추가했습니다.
    - 유연한 높이와 패딩으로 메인 콘텐츠 영역 레이아웃을 개선했습니다.
    - 푸터 아이콘 툴팁을 유지하고 레이아웃과 스타일을 개선했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,33 @@ import Image from "next/image";
 export default function Home() {
   const image = "/assets/img/logo-transparent.png";
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-10">
-      <header className="fixed top-0 left-0 w-screen h-8 flex justify-between items-center bg-neutral-800 px-2">
-        <Image src={image} alt="main logo" width={28} height={28} />
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <header className="w-screen h-8 flex justify-between items-center bg-neutral-800 px-2">
+        <span>
+          <Image src={image} alt="logo" width={24} height={24} />
+        </span>
         <span>7월 29일 (월) 오후 11:11</span>
       </header>
       {/*<MainLogo />*/}
-      <div>
-        welcome!
+      <div className="flex-grow h-full m-3 p-3 w-full flex flex-col">
+        <div className="bg-neutral-700 w-full h-8 rounded-t-xl">
+          <span className="flex">
+            <svg width="30" height="30" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="10" cy="10" r="5" fill="red"/>
+            </svg>
+            <svg width="30" height="30" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="10" cy="10" r="5" fill="yellow"/>
+            </svg>
+            <svg width="30" height="30" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="10" cy="10" r="5" fill="green"/>
+            </svg>
+          </span>
+        </div>
+        <div className="bg-neutral-600 w-full flex-grow rounded-b-xl">
+        </div>
       </div>
-      <footer className="fixed bottom-2 left-50 min-w-16 h-16 flex justify-between pt-2 px-1 items-center bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600">
+      <footer
+        className="min-w-16 h-16 flex justify-between mb-3 pt-2 px-1 items-center bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600">
         <div className="group w-14 h-full flex flex-col items-center justify-between relative">
           <div
             className="absolute bottom-[74px] text-xs rounded-lg py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600
