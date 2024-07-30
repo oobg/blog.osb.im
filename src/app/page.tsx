c54b6724ff416f5ec0fd7740aafb1f2b799c9f05
baewoong <yoonseok.bae98@gmail.com>
2024-07-31 00:25:28 +0900

    chore: KakaoTalk 아이콘을 webp에서 png로 변경
    - KakaoTalk 아이콘을 `kakaotalk.webp`에서 `kakaotalk.png`로 변경했습니다.
    - 프로젝트 내 KakaoTalk 아이콘에 대한 모든 참조를 조정했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <span>7월 29일 (월) 오후 11:11</span>
       </header>
       {/*<MainLogo />*/}
-      <div className="flex-grow h-full m-3 p-3 w-full flex flex-col">
+      <div className="flex-grow h-full p-3 w-full flex flex-col">
         <div className="bg-neutral-700 w-full h-8 rounded-t-xl">
           <span className="flex">
             <svg width="30" height="30" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
@@ -30,14 +30,17 @@ export default function Home() {
         </div>
       </div>
       <footer
-        className="min-w-16 h-16 flex justify-between mb-3 pt-2 px-1 items-center bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600">
-        <div className="group w-14 h-full flex flex-col items-center justify-between relative">
+        className="min-w-16 h-16 flex justify-between mb-3 px-1 items-center bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600">
+        <div className="group mt-2 w-14 h-full flex flex-col items-center justify-between relative">
           <div
             className="absolute bottom-[74px] text-xs rounded-lg py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600
-              after:content-[''] after:absolute after:left-1/2 after:transform after:-translate-x-1/2 after:bottom-[-16px] after:border-8 after:border-transparent after:border-t-neutral-600">
+      after:content-[''] after:absolute after:left-1/2 after:transform after:-translate-x-1/2 after:bottom-[-16px] after:border-8 after:border-transparent after:border-t-neutral-600">
             카카오톡
           </div>
-          <Image src={"/assets/img/kakaotalk.webp"} alt="kakaotalk" className="rounded" width={40} height={40}/>
+          <div className="relative w-14 h-14">
+            <Image src={"/assets/img/kakaotalk.png"} alt="kakaotalk" className="rounded" layout="fill"
+                   objectFit="cover"/>
+          </div>
           <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <circle cx="10" cy="10" r="3" fill="white"/>
           </svg>
