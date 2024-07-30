42cd8c331e40d3f2a3a98cd178055427ef7204ce
baewoong <yoonseok.bae98@gmail.com>
2024-07-31 01:01:08 +0900

    refactor: 헤더 로직을 분리하고 DisplayTime 컴포넌트 생성
    - 날짜 및 시간 표시 로직을 새로운 DisplayTime 컴포넌트로 이동했습니다.
    - Header 컴포넌트를 업데이트하여 새로운 DisplayTime 컴포넌트를 사용하도록 했습니다.
    - useEffect와 setInterval을 사용하여 날짜와 시간이 매 분마다 업데이트되도록 했습니다.

---
+++ src/features/header/ui/DisplayTime.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const DisplayTime: React.FC = () => {
+	const [dateTime, setDateTime] = useState(new Date());
+
+	useEffect(() => {
+		const now = new Date();
+		const seconds = now.getSeconds();
+		const delay = (60 - seconds) * 1000;
+
+		const initialTimeout = setTimeout(() => {
+			setDateTime(new Date());
+			const interval = setInterval(() => {
+				setDateTime(new Date());
+			}, 60000);
+
+			return () => clearInterval(interval);
+		}, delay);
+
+		return () => clearTimeout(initialTimeout);
+	}, []);
+
+	const formattedDateTime = dateTime.toLocaleDateString("ko-KR", {
+		month: "long",
+		day: "numeric",
+		weekday: "short",
+		hour: "2-digit",
+		minute: "2-digit",
+	});
+
+	return <span>{formattedDateTime}</span>;
+};
+
+export default DisplayTime;
--- src/features/header/ui/header.tsx
+++ src/features/header/ui/header.tsx
@@ -1,26 +1,16 @@
 import Image from "next/image";
+import DisplayTime from "./DisplayTime";
 
 const Header: React.FC = () => {
 	const imageSrc = "/assets/img/logo-transparent.png"; // 이미지 경로 설정
 	const altText = "logo";
-
-	const dateTime = new Date();
-	const formattedDateTime = dateTime.toLocaleDateString("ko-KR", {
-		year: "numeric",
-		month: "long",
-		day: "numeric",
-		weekday: "short",
-	}) + ` (${["일", "월", "화", "수", "목", "금", "토"][dateTime.getDay()]}) ` + dateTime.toLocaleTimeString("ko-KR", {
-		hour: "2-digit",
-		minute: "2-digit",
-	});
 
 	return (
 		<header className="w-screen h-8 flex justify-between items-center bg-neutral-800 px-2">
       <span>
         <Image src={imageSrc} alt={altText} width={24} height={24} />
       </span>
-			<span>{formattedDateTime}</span>
+			<DisplayTime />
 		</header>
 	);
 };
