242126f9f859c450ae4b52d2f4e3345e3cb24e05
baewoong <yoonseok.bae98@gmail.com>
2024-07-31 23:38:14 +0900

    refactor: Home 컴포넌트 내용을 단락 요소로 감쌈
    - Home 컴포넌트 내용을 `<p>` 요소로 감쌌습니다.
    - 소개 텍스트의 일관된 스타일과 구조를 보장했습니다.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 
 export default function Home() {
   return (
-    <>
+    <p>
       <span>Welcome My Blog! 🚀</span>
       <br />
       <span>I'm BaeWoong, a front-end developer.</span>
       <span> I'm currently working on a project called "Blog" to improve my skills.</span>
-    </>
+    </p>
   );
 }
