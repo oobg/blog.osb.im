5fbc0f64149cef46d28695fdd7abf5545a33ccd4
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-02 23:22:10 +0900

    fix: image 태그를 next/image로 변경하고 원격 이미지 호출을 위한 Next.js 설정 추가
    - 빌드 오류 해결을 위해 HTML img 태그를 Next.js next/image 컴포넌트로 교체
    - images.unsplash.com에서 이미지를 가져올 수 있도록 Next.js 설정 업데이트

--- src/shared/ui/mainCard/image.tsx
+++ src/shared/ui/mainCard/image.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
-interface cardImageProps {
+interface CardImageProps {
 	imageSrc: string;
 	imageAlt: string;
 }
 
-const CardImage: React.FC<cardImageProps> = ({ imageSrc, imageAlt }) => {
+const CardImage: React.FC<CardImageProps> = ({ imageSrc, imageAlt }) => {
 	return (
-		<div
-			className="relative m-4 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-neutral-800 shadow-neutral-800/40">
-			<img
+		<div className="relative m-4 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-neutral-800 shadow-neutral-800/40">
+			<Image
 				src={imageSrc}
 				alt={imageAlt}
+				width={300}
+				height={144}
 				className="rounded-xl object-cover h-36 w-full"
 			/>
 		</div>
