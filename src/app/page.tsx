79d082db080c2ea40393c8668810b6c458e6213d
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-02 22:32:55 +0900

    style: Home 컴포넌트에 패딩 추가
    - Home 컴포넌트를 패딩이 있는 div로 변경
    - 추가 패딩으로 레이아웃 일관성 향상

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { CardList } from "@/features/card";
 
 export default function Home() {
   return (
-    <>
+    <div className="py-5">
       <span>Welcome My Blog! 🚀</span>
       <br />
       <span>I&apos;m BaeWoong, a front-end developer.</span>
@@ -10,6 +10,6 @@ export default function Home() {
       <span> I&apos;m currently working on a project called &quot;Blog&quot; to improve my skills.</span>
       <br />
       <CardList />
-    </>
+    </div>
   );
 }
