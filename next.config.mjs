058f4b71a830a370a3737f8e4179612ecf52032b
bae woong <109705692+oobg@users.noreply.github.com>
2024-08-08 16:17:35 +0900

    feat: 외부이미지 사용시, 이미지 최적화 하지 않고 해당 도메인 직접 접근하도록 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-			}
-		],
+		// remotePatterns: [
+		// 	{
+		// 		protocol: "https",
+		// 		hostname: "images.unsplash.com",
+		// 	}
+		// ],
+		loader: "imgix",
+		path: "https://images.unsplash.com/photo-"
 	}
 };
 
