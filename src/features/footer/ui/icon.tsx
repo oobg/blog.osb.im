d5b9eab18ed927b7d44e9c5b727303eae21523e7
bae woong <109705692+oobg@users.noreply.github.com>
2024-09-25 00:13:41 +0900

    feat: footer 아이콘 및 툴팁 UI 수정

--- src/features/footer/ui/icon.tsx
+++ src/features/footer/ui/icon.tsx
@@ -10,14 +10,15 @@ const IconItem: React.FC<IconItemProps> = ({ imageSrc, label }) => {
 	return (
 		<div className="group mt-2 w-14 h-full flex flex-col items-center justify-between relative">
 			<div
-				className="absolute bottom-[74px] text-xs py-1 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap rounded-lg bg-[#333] border border-[#565656] text-[#ddd] shadow-[0_0_0_1px_#3f3f3f]"
+				className="absolute bottom-[74px] text-xs py-1 px-3 opacity-0 group-hover:opacity-100 transition-opacityduration-300
+				whitespace-nowrap rounded-lg bg-[rgba(51,51,51,0.85)] border border-[#565656] text-[#ddd] shadow-[0_0_0_1px_#3f3f3f]"
 			>
 				{label}
 			</div>
-			<div className="relative w-14 h-14 -m-1 overflow-hidden">
+			<div className="relative overflow-hidden w-14 h-14 -mt-1 -mb-2">
 				<Image src={imageSrc} alt={label} className="rounded" layout="fill" objectFit="contain" />
 			</div>
-			<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+			<svg width="16" height="16" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="10" cy="10" r="3" fill="white" />
 			</svg>
 		</div>
