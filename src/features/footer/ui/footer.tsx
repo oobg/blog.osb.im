0f3b6b5b888f9db9d4bd4166f8fa2eac3d1d1756
bae woong <109705692+oobg@users.noreply.github.com>
2024-09-27 01:42:22 +0900

    feat: footer img 변경 및 주소 이동을 위한 href 속성 추가

--- src/features/footer/ui/footer.tsx
+++ src/features/footer/ui/footer.tsx
@@ -1,16 +1,19 @@
 import IconItem from "./icon";
+import Link from "next/link";
 
 const icons = [
-	{ imageSrc: "/assets/img/kakaotalk.png", label: "카카오톡" },
-	{ imageSrc: "/assets/img/obsidian.png", label: "옵시디언" },
-	{ imageSrc: "/assets/img/github.png", label: "프로젝트" },
+	{ imageSrc: "/assets/img/osx-finder.png", label: "Home", href: "/" },
+	{ imageSrc: "/assets/img/osx-memo.png", label: "Post", href: "/posts" },
+	{ imageSrc: "/assets/img/github.png", label: "Project", href: "/project" },
 ];
 
 const Footer: React.FC = () => {
 	return (
 		<footer className="min-w-16 h-16 flex justify-between mb-2 px-1 items-center bg-neutral-600 bg-opacity-30 rounded-xl border border-neutral-600">
-			{icons.map((icon, index) => (
-				<IconItem key={index} imageSrc={icon.imageSrc} label={icon.label} />
+			{icons.map(({href, imageSrc, label}, index) => (
+				<Link href={href} key={index}>
+					<IconItem key={index} imageSrc={imageSrc} label={label} />
+				</Link>
 			))}
 		</footer>
 	);
