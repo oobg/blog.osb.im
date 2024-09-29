25d0ede9ee32cf55a45acf19fba75b5e1c1d3be7
bae woong <109705692+oobg@users.noreply.github.com>
2024-09-29 16:27:29 +0900

    refactor: 상단바 시간 표시 컴포넌트 리팩토링

--- src/features/header/ui/DisplayTime.tsx
+++ src/features/header/ui/DisplayTime.tsx
@@ -6,20 +6,20 @@ const DisplayTime: React.FC = () => {
 	const [dateTime, setDateTime] = useState(new Date());
 
 	useEffect(() => {
+		const updateDateTime = () => setDateTime(new Date());
+
 		const now = new Date();
 		const seconds = now.getSeconds();
 		const delay = (60 - seconds) * 1000;
 
 		const initialTimeout = setTimeout(() => {
-			setDateTime(new Date());
-			const interval = setInterval(() => {
-				setDateTime(new Date());
-			}, 60000);
+			updateDateTime(); // 초기 업데이트
+			const interval = setInterval(updateDateTime, 60000); // 1분마다 업데이트
 
-			return () => clearInterval(interval);
+			return () => clearInterval(interval); // 클린업 함수
 		}, delay);
 
-		return () => clearTimeout(initialTimeout);
+		return () => clearTimeout(initialTimeout); // 클린업 함수
 	}, []);
 
 	const formattedDateTime = dateTime.toLocaleDateString("ko-KR", {
@@ -30,7 +30,7 @@ const DisplayTime: React.FC = () => {
 		minute: "2-digit",
 	});
 
-	return <span>{formattedDateTime}</span>;
+	return <time dateTime={dateTime.toISOString()}>{formattedDateTime}</time>;
 };
 
 export default DisplayTime;
