c29153d8843ff375acadc8db498883a4fc19ffcb
bae woong <109705692+oobg@users.noreply.github.com>
2024-10-03 00:02:57 +0900

    build: nextra 배포를 위해 배포 스크립트 수정

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,16 @@ import nextra from "nextra";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
 	typescript: {
 		ignoreBuildErrors: true,
 	},
 	images: {
-		unoptimized: true,
-		// remotePatterns: [
-		// 	{
-		// 		protocol: "https",
-		// 		hostname: "images.unsplash.com",
-		// 	},
-		// ],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+			},
+		],
 	},
 };
 
