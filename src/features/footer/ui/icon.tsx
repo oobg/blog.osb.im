c5e18e8006911d7e72642a6d584c1d76a80c38af
bae woong <109705692+oobg@users.noreply.github.com>
2024-10-03 01:15:26 +0900

    fix: IconItem 컴포넌트에서 Image 레이아웃 및 속성 수정
    - next/image 컴포넌트에서 layout="fill"과 objectFit="contain" 제거, width(56px)와 height(56px) 속성 추가.
    - 잘못된 Tailwind CSS 클래스인 "transition-opacityduration-300"을 "transition-opacity duration-300"으로 수정.
    - 레이아웃 문제 해결 및 이미지 비율 조정을 위해 수정 작업 진행.

--- src/features/footer/ui/icon.tsx
+++ src/features/footer/ui/icon.tsx
@@ -10,13 +10,13 @@ const IconItem: React.FC<IconItemProps> = ({ imageSrc, label }) => {
 	return (
 		<div className="group mt-2 w-14 h-full flex flex-col items-center justify-between relative">
 			<div
-				className="absolute bottom-[74px] text-xs py-1 px-3 opacity-0 group-hover:opacity-100 transition-opacityduration-300
-				whitespace-nowrap rounded-lg bg-[rgba(51,51,51,0.85)] border border-[#565656] text-[#ddd] shadow-[0_0_0_1px_#3f3f3f]"
+				className="absolute bottom-[74px] text-xs py-1 px-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300
+        whitespace-nowrap rounded-lg bg-[rgba(51,51,51,0.85)] border border-[#565656] text-[#ddd] shadow-[0_0_0_1px_#3f3f3f]"
 			>
 				{label}
 			</div>
-			<div className="relative overflow-hidden w-14 h-14 -mt-1 -mb-2">
-				<Image src={imageSrc} alt={label} className="rounded" layout="fill" objectFit="contain" />
+			<div className="relative w-14 h-14 -mt-1 -mb-2">
+				<Image src={imageSrc} alt={label} width={56} height={56} className="rounded object-contain" />
 			</div>
 			<svg width="16" height="16" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 				<circle cx="10" cy="10" r="3" fill="white" />
