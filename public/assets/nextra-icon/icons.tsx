c400f5b780405535149e8c81e7e55791697b8a8d
bae woong <109705692+oobg@users.noreply.github.com>
2024-10-18 00:41:59 +0900

    feat: nextra-icon asset 추가 (docker)

--- public/assets/nextra-icon/icons.tsx
+++ public/assets/nextra-icon/icons.tsx
@@ -2,19 +2,24 @@ import Image from "next/image";
 
 interface ImageComponentProps {
 	name: string;
+	extension?: string;
 }
 
 const srcPrefix = "/assets/nextra-icon/";
 
-const ImageComponent = ({ name }: ImageComponentProps) => {
+const ImageComponent = ({ name, extension = "png" }: ImageComponentProps) => {
 	return (
-		<Image src={srcPrefix + name + ".png"} alt={name + " icon"} width={24} height={24} />
+		<Image src={srcPrefix + name + "." + extension} alt={name + " icon"} width={24} height={24} />
 	);
 }
 
 const IconNodeJS = () => {
 	return <ImageComponent name="nodejs" />;
 };
+
+const IconDocker = () => {
+	return <ImageComponent name="docker" extension="webp" />;
+}
 
 const IconProgrammers = () => {
 	return <ImageComponent name="programmers" />;
@@ -30,6 +35,7 @@ const IconCode = () => {
 
 export {
 	IconNodeJS,
+	IconDocker,
 	IconProgrammers,
 	IconText,
 	IconCode,
