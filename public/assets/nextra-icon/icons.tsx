47a86308c1acd5177f675b53d0a42ff9f4878f9b
bae woong <109705692+oobg@users.noreply.github.com>
2024-10-27 19:09:00 +0900

    feat: nextra icon width, height 옵셔널 처리

--- public/assets/nextra-icon/icons.tsx
+++ public/assets/nextra-icon/icons.tsx
@@ -4,18 +4,25 @@ import { memo, NamedExoticComponent } from "react";
 interface NextraIconProps {
 	name: string;
 	extension?: string;
+	width?: number;
+	height?: number;
 }
 
 const srcPrefix: string = "/assets/nextra-icon/";
 
 const NextraIcon: NamedExoticComponent<NextraIconProps> =
-	memo(({ name, extension = "png" }: NextraIconProps) => {
+	memo(({
+    name,
+    extension = "png",
+    width = 24,
+    height = 24,
+  }: NextraIconProps) => {
 	return (
 		<Image
 			src={`${srcPrefix}${name}.${extension}`}
 			alt={`${name} icon`}
-			width={24}
-			height={24}
+			width={width}
+			height={height}
 		/>
 	);
 });
