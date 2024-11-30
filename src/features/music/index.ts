79ec69bda117c02994c87afad2e242a275422bd3
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-01 00:06:55 +0900

    feat: Music 컴포넌트 호출하는 index.ts 생성

---
+++ src/features/music/index.ts
@@ -0,0 +1,13 @@
+import dynamic from "next/dynamic";
+
+const Logo = dynamic(() => import("./ui/Logo"));
+const MusicGrid = dynamic(() => import("./ui/MusicGrid"));
+const MusicGridCellAlbum = dynamic(() => import("./ui/MusicGridCellAlbum"));
+const SNBItem = dynamic(() => import("./ui/SNBItem"));
+
+export {
+	Logo,
+	MusicGrid,
+	MusicGridCellAlbum,
+	SNBItem,
+};
