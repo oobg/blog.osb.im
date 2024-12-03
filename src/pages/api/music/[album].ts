2f9ff23690e16cf2f3c8ae4974a5720233d2dac2
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-04 00:09:40 +0900

    feat: music api 데이터 추가 (GROWTH THEORY)

--- src/pages/api/music/[album].ts
+++ src/pages/api/music/[album].ts
@@ -36,4 +36,26 @@ const album = [
 			{ title: "기특해", duration: "3:49" },
 		],
 	},
+	{
+		id: "growth-theory",
+		album: {
+			title: "GROWTH THEORY",
+			description: "YOUNHA 7th Album",
+			kind: "Studio Album",
+		},
+		imgSrc: "7th-GROWTH THEORY.jpg",
+		imgAlt: "GROWTH THEORY Album Cover",
+		tracks: [
+			{ title: "맹그로브", duration: "2:59" },
+			{ title: "죽음의 나선", duration: "3:56" },
+			{ title: "케이프 혼", duration: "3:29" },
+			{ title: "은화", duration: "2:44" },
+			{ title: "로켓방정식의 저주", duration: "2:53" },
+			{ title: "태양물고기", duration: "3:38" },
+			{ title: "코리올리 힘", duration: "3:05" },
+			{ title: "라이프리뷰", duration: "3:10" },
+			{ title: "구름의 그림자", duration: "3:27" },
+			{ title: "새녘바람", duration: "2:57" },
+		],
+	},
 ]
