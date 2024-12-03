cedf4717dff5a91b3aa5ccbadca65223a5bfae1a
bae woong <109705692+oobg@users.noreply.github.com>
2024-12-04 00:43:21 +0900

    design: music 레이아웃 조정

--- src/features/music/ui/MusicLayout.tsx
+++ src/features/music/ui/MusicLayout.tsx
@@ -9,14 +9,16 @@ interface MusicLayoutProps {
 const MusicLayout = ({ title, children }: MusicLayoutProps) => {
 	return (
 		<div className="flex relative w-full h-full bg-[rgba(31,31,31,1)]">
-			<SNB/>
-			<div className="flex flex-col flex-1 p-6 gap-4 overflow-y-auto">
-				<h1 className="text-white text-3xl font-bold">{title}</h1>
-				<hr className="border border-[#2E2E2E]"/>
-				{children}
+			<SNB />
+			<div className="flex flex-col flex-1 py-6 gap-4 overflow-hidden">
+				<h1 className="text-white text-3xl font-bold mx-6">{title}</h1>
+				<hr className="border border-[#2E2E2E] mx-6" />
+				<div className="flex-1 overflow-y-auto px-6">
+					{children}
+				</div>
 			</div>
 		</div>
 	);
-}
+};
 
 export default MusicLayout;
