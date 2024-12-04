de342c65ac8d5cb759a6db35b7e79833335796b4
bae woong <yoonseok.bae98@dotincorp.com>
2024-12-04 09:34:42 +0900

    feat: posts 를 제외한 나머지 페이지는 nextra 메뉴에서 숨기기

--- src/pages/_meta.tsx
+++ src/pages/_meta.tsx
@@ -1,12 +1,14 @@
 export default {
   "*": {
+    display: "hidden",
     theme: {
       pagination: false,
       toc: true,
-    }
+    },
   },
   posts: {
     title: "Posts",
+    display: "list",
     type: "page",
     theme: {
       footer: false,
