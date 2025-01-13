8e33bf3c1a6fbddacf80679fff117bdae939b76a
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-13 23:55:20 +0900

    feat: Nextra Docs 목차를 _meta.tsx 데이터를 기반으로 동적 생성

---
+++ src/shared/ui/nextra/CustomCard.tsx
@@ -0,0 +1,51 @@
+import { Cards } from "nextra/components";
+import NextraIcon from "@/../public/assets/nextra-icon/icons";
+
+interface IMetaItem {
+	title: string;
+}
+
+interface IMeta {
+	[key: string]: IMetaItem | undefined;
+}
+
+interface CustomCardProps {
+	_meta: IMeta;
+	href: string;
+	iconName: string;
+	iconExtension?: string | undefined;
+	width?: number | undefined;
+	height?: number | undefined;
+}
+
+export default function CustomCard({
+	_meta,
+	href,
+	iconName,
+	iconExtension = undefined,
+	width = undefined,
+	height = undefined,
+}: CustomCardProps): JSX.Element {
+	const filteredMeta = Object.keys(_meta).filter((key) => key !== "*");
+
+	return (
+		<Cards>
+			{filteredMeta.map((key, index) => {
+				const metaItem = _meta[key];
+
+				if (!metaItem) return null;
+
+				return (
+					<Cards.Card
+						key={index}
+						title={metaItem.title}
+						href={`/posts/${href}/${key}`}
+						icon={<NextraIcon name={iconName} extension={iconExtension} width={width} height={height} />}
+						children=""
+						arrow
+					/>
+				);
+			})}
+		</Cards>
+	);
+}
