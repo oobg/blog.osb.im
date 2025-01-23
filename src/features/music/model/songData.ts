11fcc6d14031ffeec5ae1e2acfe6bc382dad4b89
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-24 00:08:14 +0900

    update: youtube music 데이터 추가중 (YOUNHA 7th Album Repackage ‘GROWTH THEORY : Final Edition’)

--- src/features/music/model/songData.ts
+++ src/features/music/model/songData.ts
@@ -2,14 +2,61 @@ import type { AlbumData } from "@/features/music/lib/types";
 
 const songData = [
 	{
-		id: "죽음의 나선",
 		albumId: "growth-theory-repackage",
-		youtubeId: "8oPnBKTCPW0"
-	},
-	{
-		id: "맹그로브",
-		albumId: "growth-theory-repackage",
-		youtubeId: "The_R7jYQ8o"
+		songs: [
+			{
+				id: "죽음의 나선",
+				youtubeId: "8oPnBKTCPW0"
+			},
+			{
+				id: "맹그로브",
+				youtubeId: "The_R7jYQ8o"
+			},
+			{
+				id: "은화",
+				youtubeId: "XuFxbZmOfG0"
+			},
+			{
+				id: "퀘이사",
+				youtubeId: "nIVWsVFtPOk"
+			},
+			{
+				id: "코리올리 힘",
+				youtubeId: "RXkMgbrEiRY"
+			},
+			{
+				id: "태양물고기",
+				youtubeId: "I3wPjqoWCMY"
+			},
+			{
+				id: "로켓방정식의 저주",
+				youtubeId: "zT9RxtAyT2s"
+			},
+			{
+				id: "포인트 니모",
+				youtubeId: "G19Gdxd9vwg"
+			},
+			{
+				id: "케이프 혼",
+				youtubeId: "wABPLDACd8M"
+			},
+			{
+				id: "라이프리뷰",
+				youtubeId: "h66BlMPrrxM"
+			},
+			{
+				id: "구름의 그림자",
+				youtubeId: "uqUA9XcrqIM"
+			},
+			{
+				id: "새녘바람",
+				youtubeId: "IpmLop9jLrA"
+			},
+			{
+				id: "기특해",
+				youtubeId: "30B2mLgg3Pw"
+			}
+		]
 	},
 ];
 
