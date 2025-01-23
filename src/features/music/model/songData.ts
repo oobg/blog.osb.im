9dcd4e7a0f1e8f48d057b20cdabe802bcb1dcd20
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-23 23:58:49 +0900

    chore: youtube music 데이터 생성

---
+++ src/features/music/model/songData.ts
@@ -0,0 +1,16 @@
+import type { AlbumData } from "@/features/music/lib/types";
+
+const songData = [
+	{
+		id: "죽음의 나선",
+		albumId: "growth-theory-repackage",
+		youtubeId: "8oPnBKTCPW0"
+	},
+	{
+		id: "맹그로브",
+		albumId: "growth-theory-repackage",
+		youtubeId: "The_R7jYQ8o"
+	},
+];
+
+export default songData;
