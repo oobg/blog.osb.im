983165e589c24f24433980b708188d8b08756f61
bae woong <109705692+oobg@users.noreply.github.com>
2025-01-25 00:31:55 +0900

    chore: songData 포맷팅

--- src/features/music/model/songData.ts
+++ src/features/music/model/songData.ts
@@ -4,104 +4,35 @@ const songData = [
 	{
 		albumId: "growth-theory-repackage",
 		songs: [
-			{
-				id: "죽음의 나선",
-				youtubeId: "8oPnBKTCPW0"
-			},
-			{
-				id: "맹그로브",
-				youtubeId: "The_R7jYQ8o"
-			},
-			{
-				id: "은화",
-				youtubeId: "XuFxbZmOfG0"
-			},
-			{
-				id: "퀘이사",
-				youtubeId: "nIVWsVFtPOk"
-			},
-			{
-				id: "코리올리 힘",
-				youtubeId: "RXkMgbrEiRY"
-			},
-			{
-				id: "태양물고기",
-				youtubeId: "I3wPjqoWCMY"
-			},
-			{
-				id: "로켓방정식의 저주",
-				youtubeId: "zT9RxtAyT2s"
-			},
-			{
-				id: "포인트 니모",
-				youtubeId: "G19Gdxd9vwg"
-			},
-			{
-				id: "케이프 혼",
-				youtubeId: "wABPLDACd8M"
-			},
-			{
-				id: "라이프리뷰",
-				youtubeId: "h66BlMPrrxM"
-			},
-			{
-				id: "구름의 그림자",
-				youtubeId: "uqUA9XcrqIM"
-			},
-			{
-				id: "새녘바람",
-				youtubeId: "IpmLop9jLrA"
-			},
-			{
-				id: "기특해",
-				youtubeId: "30B2mLgg3Pw"
-			}
-		]
+			{ id: "죽음의 나선", youtubeId: "8oPnBKTCPW0" },
+			{ id: "맹그로브", youtubeId: "The_R7jYQ8o" },
+			{ id: "은화", youtubeId: "XuFxbZmOfG0" },
+			{ id: "퀘이사", youtubeId: "nIVWsVFtPOk" },
+			{ id: "코리올리 힘", youtubeId: "RXkMgbrEiRY" },
+			{ id: "태양물고기", youtubeId: "I3wPjqoWCMY" },
+			{ id: "로켓방정식의 저주", youtubeId: "zT9RxtAyT2s" },
+			{ id: "포인트 니모", youtubeId: "G19Gdxd9vwg" },
+			{ id: "케이프 혼", youtubeId: "wABPLDACd8M" },
+			{ id: "라이프리뷰", youtubeId: "h66BlMPrrxM" },
+			{ id: "구름의 그림자", youtubeId: "uqUA9XcrqIM" },
+			{ id: "새녘바람", youtubeId: "IpmLop9jLrA" },
+			{ id: "기특해", youtubeId: "30B2mLgg3Pw" },
+		],
 	},
 	{
 		albumId: "growth-theory",
 		songs: [
-			{
-				id: "맹그로브",
-				youtubeId: "GsY9XSwZmxg"
-			},
-			{
-				id: "죽음의 나선",
-				youtubeId: "Kv_zcoq6190"
-			},
-			{
-				id: "케이프 혼",
-				youtubeId: "MvWEe3Q89GE"
-			},
-			{
-				id: "은화",
-				youtubeId: "UzFh_xQgkxs"
-			},
-			{
-				id: "로켓방정식의 저주",
-				youtubeId: "rVuq_24eEMs"
-			},
-			{
-				id: "태양물고기",
-				youtubeId: "3VwfUp1UYqw"
-			},
-			{
-				id: "코리올리 힘",
-				youtubeId: "CfJQpm8UZWo"
-			},
-			{
-				id: "라이프리뷰",
-				youtubeId: "BX1eb-NGO0Q"
-			},
-			{
-				id: "구름의 그림자",
-				youtubeId: "GiyZ0yQ4090"
-			},
-			{
-				id: "새녘바람",
-				youtubeId: "4Juw2Vi2p6A"
-			},
-		]
+			{ id: "맹그로브", youtubeId: "GsY9XSwZmxg" },
+			{ id: "죽음의 나선", youtubeId: "Kv_zcoq6190" },
+			{ id: "케이프 혼", youtubeId: "MvWEe3Q89GE" },
+			{ id: "은화", youtubeId: "UzFh_xQgkxs" },
+			{ id: "로켓방정식의 저주", youtubeId: "rVuq_24eEMs" },
+			{ id: "태양물고기", youtubeId: "3VwfUp1UYqw" },
+			{ id: "코리올리 힘", youtubeId: "CfJQpm8UZWo" },
+			{ id: "라이프리뷰", youtubeId: "BX1eb-NGO0Q" },
+			{ id: "구름의 그림자", youtubeId: "GiyZ0yQ4090" },
+			{ id: "새녘바람", youtubeId: "4Juw2Vi2p6A" },
+		],
 	},
 	{
 		albumId: "end-theory-repackage",
